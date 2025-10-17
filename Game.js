4a63c4fe96b8f0d940818b28a9bb3dedee2b32e3
ERIC TOMAS ZANCANARO <eric.zancanaro@edu.sc.senai.br>
2025-10-17 09:48:12 -0300

    Splits the code into js and css files

---
+++ Game.js
@@ -0,0 +1,199 @@
+//Control
+const canvasId = 'space_wars_canvas';
+const canvasBufferId = 'space_wars_canvas_bf';
+const shipRadius = 10;
+const ROTATION_SPEED = 0.03;
+const KEYS_PRESSED = {};
+const STAR_POSITION = { x: 150, y: 150 }
+const STAR_RADIUS = 30;
+const STAR_COLOR = '#F5A927';
+const TOP_SPEED = 10;
+const MAX_ACCELERATION = 2;
+const ACCELERATION_DELTA = 0.02;
+
+let renderer;
+let buffer = document.createElement('canvas');
+let renderCtx;
+let bufferCtx;
+let needle_audit = { x: 0, y: 0, a: 0 };
+let wedge_audit = { x: 0, y: 0, a: 0 };
+
+//ships
+let needle = {
+    position: { x: 10, y: 10 },
+    torpedoes: 10,
+    rotation: 0,
+    rotation_dir: 0,
+    acceleration: 0,
+    color: '#E427F5'
+};
+let wedge = {
+    position: { x: 100, y: 100 },
+    torpedoes: 10,
+    rotation: Math.PI,
+    rotation_dir: 0,
+    acceleration: 0,
+    color: '#2798F5'
+};
+
+function init_audit() {
+    needle_audit.x = document.getElementById('needle_x');
+    needle_audit.y = document.getElementById('needle_y');
+    needle_audit.a = document.getElementById('needle_a');
+    wedge_audit.x = document.getElementById('wedge_x');
+    wedge_audit.y = document.getElementById('wedge_y');
+    wedge_audit.a = document.getElementById('wedge_a');
+}
+
+function init_canvas() {
+    renderer = document.getElementById(canvasId);
+    buffer.width = renderer.width;
+    buffer.height = renderer.height;
+    bufferCtx = buffer.getContext("2d");
+    renderCtx = renderer.getContext("2d");
+    window.requestAnimationFrame(render);
+    init_audit();
+}
+
+function draw_star() {
+    bufferCtx.save();
+    bufferCtx.fillStyle = STAR_COLOR;
+    bufferCtx.beginPath();
+    bufferCtx.arc(STAR_POSITION.x, STAR_POSITION.y, STAR_RADIUS, 0, Math.PI * 2);
+    bufferCtx.fill();
+    bufferCtx.restore();
+}
+
+function draw_ship(ship) {
+    bufferCtx.save();
+    bufferCtx.fillStyle = ship.color;
+    bufferCtx.translate(ship.position.x, ship.position.y);
+    bufferCtx.rotate(ship.rotation);
+
+    bufferCtx.beginPath();
+    bufferCtx.moveTo(shipRadius, 0);
+    bufferCtx.lineTo(-shipRadius, 0);
+    bufferCtx.lineTo(-shipRadius, shipRadius);
+    bufferCtx.fill();
+    bufferCtx.lineTo(-shipRadius, -shipRadius);
+    bufferCtx.fill();
+
+    bufferCtx.restore();
+}
+function rotate_ship(ship) {
+    if (ship.rotation_dir != 0) {
+        ship.rotation += ship.rotation_dir * ROTATION_SPEED;
+    }
+}
+
+function move_ship(ship) {
+    //STAR_GRAVITY_PULL
+    //SHIP_THRUSTERS
+    let direction_vec = { x: Math.cos(ship.rotation), y: Math.sin(ship.rotation) };
+    ship.position.x += direction_vec.x * ship.acceleration;
+    ship.position.y += direction_vec.y * ship.acceleration;
+}
+
+function update_ship_position(ship) {
+    rotate_ship(ship);
+    move_ship(ship);
+}
+
+function render_ships() {
+    update_ship_position(needle);
+    update_ship_position(wedge);
+    draw_ship(needle);
+    draw_ship(wedge);
+}
+
+function render() {
+    bufferCtx.fillStyle = "rgb(255 255 255 / 30%)";
+    bufferCtx.fillRect(0, 0, buffer.width, buffer.height);
+    //bufferCtx.clearRect(0, 0, 300, 300);
+    handle_pressed_keys();
+    draw_star();
+    render_ships();
+
+    renderCtx.drawImage(buffer, 0, 0);
+    set_audit();
+
+    window.requestAnimationFrame(render);
+}
+
+function rotate(modifier) {
+    needle.rotation += modifier * ROTATION_SPEED;
+    wedge.rotation += modifier * ROTATION_SPEED;
+}
+
+/**
+ * Sets the ship acceleration with given magnitude.
+ * Acceleration starts slow and increases faster the longer the key is pressed
+ * Should go from 0 up to MAX_ACCELERATION. 
+**/
+function set_acceleration(ship, magnitude) {
+    ship.acceleration += magnitude * ACCELERATION_DELTA;
+    if (ship.acceleration > MAX_ACCELERATION) {
+        ship.acceleration = MAX_ACCELERATION;
+    } else if (ship.acceleration < 0) {
+        ship.acceleration = 0;
+    }
+}
+
+
+function set_rotation(ship, direction) {
+    if (ship.rotation_dir == direction) {
+        return;
+    }
+    if (ship.rotation_dir == -direction) {
+        ship.rotation_dir = 0;
+    } else {
+        ship.rotation_dir = direction;
+    }
+}
+
+function unset_rotation(ship, direction) {
+    if (ship.rotation_dir == direction) {
+        ship.rotation_dir = 0;
+    } else if (ship.rotation_dir == -direction) {
+        return;
+    }
+}
+
+function set_audit() {
+    needle_audit.x.innerHTML = needle.position.x;
+    wedge_audit.x.innerHTML = wedge.position.x;
+    needle_audit.y.innerHTML = needle.position.y;
+    wedge_audit.y.innerHTML = wedge.position.y;
+    needle_audit.a.innerHTML = needle.acceleration;
+    wedge_audit.a.innerHTML = wedge.acceleration;
+}
+
+function handle_pressed_keys() {
+    Object.keys(KEYS_PRESSED).forEach(key => {
+        console.log(key);
+        switch (key) {
+            case 'a': set_rotation(needle, -1); break;
+            case 'd': set_rotation(needle, 1); break;
+            case 'w': set_acceleration(needle, 1); break;
+            case 'j': set_rotation(wedge, -1); break;
+            case 'l': set_rotation(wedge, 1); break;
+            case 'i': set_acceleration(wedge, 1); break;
+        }
+    });
+}
+
+function key_pressed(event) {
+    KEYS_PRESSED[event.key] = true;
+}
+
+function key_released(event) {
+    delete KEYS_PRESSED[event.key];
+    switch (event.key) {
+        case 'a': unset_rotation(needle, -1); break;
+        case 'd': unset_rotation(needle, 1); break;
+        case 'w': set_acceleration(needle, 0); break;
+        case 'j': unset_rotation(wedge, -1); break;
+        case 'l': unset_rotation(wedge, 1); break;
+        case 'i': set_acceleration(wedge, 0); break;
+    }
+}
