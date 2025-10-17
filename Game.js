b827b5a4871b69a0f8bb6df2fc3b2b4cd6891268
ERIC TOMAS ZANCANARO <eric.zancanaro@edu.sc.senai.br>
2025-10-17 14:20:39 -0300

    Adds collision detection for a pair of lines. Applies the function to check ship-boundary collision

--- Game.js
+++ Game.js
@@ -1,7 +1,7 @@
 //Control
-const canvasId = 'space_wars_canvas';
-const canvasBufferId = 'space_wars_canvas_bf';
-const shipRadius = 10;
+const CANVAS_ID = 'space_wars_canvas';
+const CANVAS_BUFFER_ID = 'space_wars_canvas_bf';
+const SHIP_RADIUS = 10;
 const ROTATION_SPEED = 0.03;
 const KEYS_PRESSED = {};
 const STAR_POSITION = { x: 150, y: 150 }
@@ -20,7 +20,7 @@ let wedge_audit = { x: 0, y: 0, a: 0 };
 
 //ships
 let needle = {
-    position: { x: 10, y: 10 },
+    position: { x: 20, y: 20 },
     torpedoes: 10,
     rotation: 0,
     rotation_dir: 0,
@@ -42,6 +42,10 @@ let wedge = {
 
 const INITIAL_STATE = { needle_start: needle, wedge_start: wedge };
 
+function init_ships(){
+
+}
+
 function init_audit() {
     needle_audit.x = document.getElementById('needle_x');
     needle_audit.y = document.getElementById('needle_y');
@@ -52,12 +56,17 @@ function init_audit() {
 }
 
 function init_canvas() {
-    renderer = document.getElementById(canvasId);
+    renderer = document.getElementById(CANVAS_ID);
     buffer.width = renderer.width;
     buffer.height = renderer.height;
     bufferCtx = buffer.getContext("2d");
     renderCtx = renderer.getContext("2d");
     window.requestAnimationFrame(render);
+}
+
+function init(){
+    init_canvas();
+    init_ships();
     init_audit();
 }
 
@@ -83,17 +92,73 @@ function draw_ship(ship) {
     if (ship.exploded) {
         draw_explosion();
     } else {
+        let vertices = get_ship_vertices({ x: 0, y: 0 });
         bufferCtx.beginPath();
-        bufferCtx.moveTo(shipRadius, 0);
-        bufferCtx.lineTo(-shipRadius, 0);
-        bufferCtx.lineTo(-shipRadius, shipRadius);
+        bufferCtx.moveTo(vertices[0].x, 0);
+        // bufferCtx.lineTo(-SHIP_RADIUS, 0);
+        bufferCtx.lineTo(vertices[1].x, vertices[1].y);
         bufferCtx.fill();
-        bufferCtx.lineTo(-shipRadius, -shipRadius);
+        bufferCtx.lineTo(vertices[2].x, vertices[2].y);
         bufferCtx.fill();
     }
 
     bufferCtx.restore();
 }
+
+function check_ship_boundary_collision(ship) {
+    let v = get_ship_vertices(ship.position);
+    let boundary_lines = [
+        { point1: { x: 0, y: 0 }, point2: { x: 0, y: renderer.height } },
+        { point1: { x: 0, y: 0 }, point2: { x: renderer.width, y: 0 } },
+        { point1: { x: renderer.width, y: 0 }, point2: { x: renderer.width, y: renderer.height } },
+        { point1: { x: 0, y: renderer.height }, point2: { x: renderer.width, y: renderer.height } },
+    ]
+    let ship_lines = [
+        { point1: { x: v[0].x, y: v[0].y }, point2: { x: v[1].x, y: v[1].y } },
+        { point1: { x: v[0].x, y: v[0].y }, point2: { x: v[2].x, y: v[2].y } },
+        { point1: { x: v[1].x, y: v[1].y }, point2: { x: v[2].x, y: v[2].y } },
+    ]
+    for (let line of ship_lines) {
+        for (let bound of boundary_lines) {
+            if (check_lines_collision(line.point1, line.point2, bound.point1, bound.point2)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+/**
+ * Checks collision between 2 lines.
+ * Line 1 is from point1 to point2.
+ * Line 2 is from point3 to point4.
+ */
+function check_lines_collision(point1, point2, point3, point4) {
+
+    let divisor =
+        ((point4.y - point3.y) * (point2.x - point1.x)
+            - (point4.x - point3.x) * (point2.y - point1.y))
+    let cpA =
+        ((point4.x - point3.x) * (point1.y - point3.y)
+            - (point4.y - point3.y) * (point1.x - point3.x))
+        / divisor;
+    let cpB =
+        ((point1.x - point1.x) * (point1.y - point3.y)
+            - (point2.y - point1.y) * (point1.x - point3.x))
+        / divisor;
+
+    return cpA >= 0 && cpA <= 1 && cpB >= 0 && cpB <= 1;
+}
+
+function get_ship_vertices(base_position) {
+    return [
+        { x: base_position.x + SHIP_RADIUS, y: base_position.y },
+        { x: base_position.x - SHIP_RADIUS, y: base_position.y + SHIP_RADIUS },
+        { x: base_position.x - SHIP_RADIUS, y: base_position.y - SHIP_RADIUS },
+    ]
+}
+
 function rotate_ship(ship) {
     if (ship.rotation_dir != 0) {
         ship.rotation += ship.rotation_dir * ROTATION_SPEED;
@@ -147,15 +212,27 @@ function rotate(modifier) {
     wedge.rotation += modifier * ROTATION_SPEED;
 }
 
+
 /**
  * Handles collisions between game objects.
  * 1. Check for ships colliding with the boundary;
- * 2. Check for ships colliding with themselves;
- * 3. Check for ships colliding with the star;
+ * 2. Check for ships colliding with the star;
  */
-function handle_collisions(ship) {
+function handle_object_collisions(ship) {
+    if(check_ship_boundary_collision(ship)){
+        console.log("SHIP IS TOUCHING BOUNDS!");
+        return true;
+    }
+    //TO-DO: Check for collision between Ship and Star
+}
+
+/**
+ * Check if ships collide with each other
+ */
+function handle_ships_collision(){
 
 }
+
 
 /**
  * Sets the ship acceleration with given magnitude.
@@ -233,6 +310,6 @@ function key_released(event) {
 /**
  * Resets the game to INITIAL_STATE
  */
-function game_reset(){
+function game_reset() {
 
 }
